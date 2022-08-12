04272391a25229b7e2c361a8bf74962cbaf42299
Benton Tripp <benton.tripp@elderresearch.com>
2022-08-12 09:51:32 -0400

    basic distinct row/columns

--- jigsaw_doku/jigsaw_values.py
+++ jigsaw_doku/jigsaw_values.py
@@ -1,12 +1,40 @@
-import random
-from functools import reduce
+from sympy.utilities.iterables import multiset_permutations
+import numpy as np
 
-from jigsaw_board import *
-from utils import *
+
+
 
 class SudokuValues(JigsawSudoku):
 
-    def __init__(self):
+    def __init__(self, size=9):
         # Maybe just try generating the puzzles WITH the values, and use that as an additional constraint? Or try generating values, shuffling, and creating regions when a 
         # possible solution is found?
-        pass
+        self.size = size
+        # possible rows
+        pr = self.perms()
+        # start out with board that has unique rows/column values
+        for i in range(0, self.size):
+            if i == 0:
+                m = pr.pop(0).reshape(1, self.size)
+            else:
+                m, pr = update_grid(m, pr) 
+                
+
+    def reshape_list_to_arr(self, perm):
+        return np.reshape(np.array(perm), (1, self.size))
+
+    def perms(self):
+        p = [reshape_list_to_arr(perm, self.size) for perm in multiset_permutations(np.arange(1, self.size + 1))]
+        np.random.shuffle(p)
+        return p
+
+
+    def update_grid(self, m, pr):
+        for i in range(0, len(pr)):
+            cm = np.concatenate((m, pr[i]), axis=0)
+            if len([col for col in range(0, self.size) if np.unique(cm[:, col]).size == cm.shape[0]]) == self.size:
+                pr.pop(i)
+                return cm, pr
+            else:
+                pr.pop(i)
+            
